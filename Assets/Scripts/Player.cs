ddec9cfa79cbe36093890fd6e28b37fd48c806a3
Tommy Tran <tblyduc2412@gmail.com>
2024-01-04 23:15:44 +0700

    feat: Wall Slide & Wall Jump.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,9 +7,10 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider2D;
 
+    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float jumpPower = 10f;
-    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float wallSlidingSpeed = 2f;
 
     private float jumpBufferTimer;
     private float jumpBufferTimerMax = 0.2f;
@@ -24,6 +26,7 @@ public class Player : MonoBehaviour
         Jump,
         Fall,
         DoubleJump,
+        WallSlide,
         WallJump,
         Hit,
     }
@@ -46,22 +49,31 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        HandleMovement();
+        Movement();
         Jumping();
+        WallSliding();
+        WallJumping();
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpNumber -= 1;
+
+        }
     }
 
-    private void HandleMovement()
+    private void FixedUpdate()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    }
+
+    private void Movement()
+    {
+        if (state != State.WallJump && state != State.WallSlide)
+            rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
 
         // Flip
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
+            Flip();
         }
 
         // Player State
@@ -72,6 +84,14 @@ public class Player : MonoBehaviour
             else
                 state = State.Idle;
         }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
     }
 
     private void Jumping()
@@ -95,16 +115,11 @@ public class Player : MonoBehaviour
             jumpBufferTimer -= Time.deltaTime;
         }
 
-        if (Input.GetButtonUp("Jump"))
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
-            if (rb.velocity.y > 0f)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.7f); // Decrease jump power when quick jump
-            }
-
-            jumpBufferTimer = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.7f); // Decrease jump power when quick jump
             coyoteTimer = 0f;
-            jumpNumber -= 1;
+            jumpBufferTimer = 0f;
         }
 
         // Ground jumping
@@ -127,12 +142,56 @@ public class Player : MonoBehaviour
         // Player State
         if (!IsGrounded())
         {
-            if (rb.velocity.y > 0f && state != State.DoubleJump)
+            if (rb.velocity.y > 0f && state != State.DoubleJump && state != State.WallJump)
                 state = State.Jump;
             else if (rb.velocity.y < 0f)
             {
                 state = State.Fall;
             }
+        }
+    }
+
+    private void WallSliding()
+    {
+        if (IsFacingWall() && horizontal != 0f && !IsGrounded())
+        {
+            rb.velocity = new Vector2(0f, Mathf.Max(-wallSlidingSpeed, rb.velocity.y));
+            state = State.WallSlide;
+        }
+    }
+
+    private void WallJumping()
+    {
+        if (state == State.WallSlide)
+        {
+            coyoteTimer = coyoteTimerMax;
+            jumpNumber = jumpNumberMax;
+        }
+        else
+            coyoteTimer -= Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump"))
+            jumpBufferTimer = jumpBufferTimerMax;
+        else
+            jumpBufferTimer -= Time.deltaTime;
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            coyoteTimer = 0f;
+            jumpBufferTimer = 0f;
+        }
+
+        Vector2 wallJumpDirection;
+        if (isFacingRight)
+            wallJumpDirection = Vector2.left;
+        else
+            wallJumpDirection = Vector2.right;
+
+        if (state == State.WallSlide && coyoteTimer > 0f && jumpBufferTimer > 0f && jumpNumber > 0)
+        {
+            rb.velocity = new Vector2(jumpPower * wallJumpDirection.x, jumpPower);
+            Flip();
+            state = State.WallJump;
         }
     }
 
@@ -157,6 +216,36 @@ public class Player : MonoBehaviour
         }
         Debug.DrawRay(boxCollider2D.bounds.max, Vector3.down * (boxCollider2D.bounds.size.y + distance), color);
 
+        return raycastHit2D.collider != null;
+    }
+
+    public bool IsFacingWall()
+    {
+        float distance = 0.1f;
+        Vector2 direction;
+
+        if (isFacingRight)
+        {
+            direction = Vector2.right;
+        }
+        else
+        {
+            direction = Vector2.left;
+        }
+
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, direction, distance, groundLayer);
+
+        Color color;
+        if (raycastHit2D.collider != null)
+        {
+            color = Color.green;
+        }
+        else
+        {
+            color = Color.red;
+        }
+
+        Debug.DrawRay(new Vector2(boxCollider2D.bounds.center.x, boxCollider2D.bounds.center.y), direction * (boxCollider2D.bounds.extents.x + distance), color);
         return raycastHit2D.collider != null;
     }
 }
--- Assets/Scripts/PlayerVisual.cs
+++ Assets/Scripts/PlayerVisual.cs
@@ -9,6 +9,8 @@ public class PlayerVisual : MonoBehaviour
     private const string IS_JUMP = "IsJump";
     private const string IS_FALL = "IsFall";
     private const string IS_DOUBLE_JUMP = "IsDoubleJump";
+    private const string IS_WALL_SLIDE = "IsWallSlide";
+
 
     [SerializeField] private Player player;
 
@@ -35,7 +37,11 @@ public class PlayerVisual : MonoBehaviour
             case Player.State.DoubleJump:
                 animator.SetTrigger(IS_DOUBLE_JUMP);
                 break;
+            case Player.State.WallSlide:
+                animator.SetTrigger(IS_WALL_SLIDE);
+                break;
             case Player.State.WallJump:
+                animator.SetTrigger(IS_JUMP);
                 break;
             case Player.State.Fall:
                 animator.SetTrigger(IS_FALL);
