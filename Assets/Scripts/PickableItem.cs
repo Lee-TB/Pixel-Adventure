626ec3692e3040aa13398e58523556d8cb2389e1
Tommy Tran <tblyduc2412@gmail.com>
2024-01-08 13:25:51 +0700

    refactor: create new pickableItem scripts using OnTriggerEnter2D.

--- Assets/Scripts/PickableItem.cs
+++ Assets/Scripts/PickableItem.cs
@@ -3,32 +3,18 @@ using UnityEngine;
 public class PickableItem : MonoBehaviour
 {
     private const string IS_COLLECTED = "IsCollected";
-    [SerializeField] LayerMask pickableLayer;
     private Animator animator;
-    private new Renderer renderer;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        renderer = GetComponent<Renderer>();
     }
 
-    private void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (IsPlayerPickup())
+        if (other.TryGetComponent(out Player player))
         {
             animator.SetTrigger(IS_COLLECTED);
         }
-    }
-
-    private bool IsPlayerPickup()
-    {
-        float radius = 0.25f;
-        Vector2 direction = Vector2.zero;
-        float distance = 0f;
-
-        RaycastHit2D raycastHit2D = Physics2D.CircleCast(renderer.bounds.center, radius, direction, distance, pickableLayer);
-
-        return raycastHit2D.collider != null;
     }
 }
