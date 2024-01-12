9d60f268f8e916c39c01e12db58d9d5cfd16cb0c
Tommy Tran <tblyduc2412@gmail.com>
2024-01-12 10:04:03 +0700

    feat: Trampoline

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    private const string IS_JUMP = "IsJump";
+    [SerializeField] private float jumpPower = 15f;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out Player player))
+        {
+            player.Jump(jumpPower);
+            animator.SetTrigger(IS_JUMP);
+        }
+    }
+}
